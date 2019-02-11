c2ffe4fb8da544d5cc08e9af39303eb669985013
Art Bachmann <bachmann.art@gmail.com>
2019-02-11 11:26:05 +0200

    es6 export/import completed

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,1 +1,11 @@
-console.log('hello graphql')
+//import myLocation, { message, name, greeting } from './myModule'
+import add, { subtract } from './math'
+
+// console.log(message)
+// console.log(name)
+// console.log(myLocation)
+
+// console.log(greeting('Aksel'))
+
+console.log(add(1, 2))
+console.log(subtract(2, 1))
---
+++ basics/src/math.js
@@ -0,0 +1,9 @@
+const add = (a, b) => {
+  return a + b
+}
+
+const subtract = (c, d) => {
+  return c - d
+}
+
+export { add as default, subtract }
